dc5a8c4482b3c0731aecc6511b354fe072d08358
chetan saini <ch3tan5aini@gmail.com>
2024-10-30 20:20:03 +0530

    Added DeleteBook and Updated Spinner

--- frontend/components/Spinner.jsx
+++ frontend/components/Spinner.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Spinner = () => {
     return (
-        <div className='animate-ping h-16 w-16 m-8 rounded-full bg-sky-600'></div>
+        <div className="flex items-center justify-center h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500"></div>
+      </div>
     )
 }
 
--- frontend/pages/DeleteBook.jsx
+++ frontend/pages/DeleteBook.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import axios from 'axios'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
 
 const DeleteBook = () => {
+  const [Loading, setLoading] = useState(false);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  function handleDeleteBook() {
+    setLoading(true);
+    axios.delete(`http://localhost:3000/books/${id}`)
+      .then(() => {
+        setLoading(false)
+        navigate("/")
+      }).catch(error => {
+        console.log(error);
+        setLoading(true);
+      })
+  }
+
+
   return (
-    <div>
-      Inside DeleteBook
+    <div className='p-4'>
+      <BackButton />
+      <h1 className="text-3xl my-4 text-center">Delete Book</h1>
+      {Loading ? <Spinner /> : ''}
+      <div className="flex flex-col item-center border-2 rounded-xl w-1/3 p-8 mx-auto">
+        <h2 className='text-2xl text-center'>Are You Sure? You Want To This Book.</h2>
+        <button className="p-4 bg-red-600 text-white mt-8 w-full" onClick={handleDeleteBook}>Delete Book</button>
+      </div>
     </div>
   )
 }
